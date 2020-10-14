3f1f341d95a6d3292d110a77d9a344b2ee0999aa
ishitarastogi <ishita3099@gmail.com>
2020-10-14 20:54:35 +0530

    Request headers and responses

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-const http= require('http');  //http module can be used globally...if absolut or relative path
+const http= require('http');  //http module is a core module of Node.js
+//http module can be used globally...if absolut or relative path
                              
-const server=http.createServer((req,res) => { //createserver method: Request listner(take two arguments)
-                                            // Request and rsponse
-                                            // return server
+const server=http.createServer((req,res) => { // The request object can be used to get information 
+                                              //about the current HTTP request e.g., url, request header, and data. 
+                                             // The response object can be used to send a response for a current HTTP request. 
     console.log(req.url, req.method, req.headers);  //2. method:GET url:/ but if we change to /Test
-                                                          // /test
-    
+   
+    // /test
+    res.setHeader('content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Node.js server</title></head>');
+    res.write('<body>Hey!!This is Ishita. I created my first node.js server.</body>')
+    res.write('</html>');   
+    res.end();//can't write anything after this
+              //open developer tools -> network -> responses->headers
 });
+
 server.listen(3000);
