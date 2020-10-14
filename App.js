85921e6e66db7168ebc0a031e23909455231e3c0
ishitarastogi <ishita3099@gmail.com>
2020-10-14 16:47:51 +0530

    understanding Request

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ const http= require('http');  //http module can be used globally...if absolut or
 const server=http.createServer((req,res) => { //createserver method: Request listner(take two arguments)
                                             // Request and rsponse
                                             // return server
-    console.log(req);
-    process.exit();
+    console.log(req.url, req.method, req.headers);  //2. method:GET url:/ but if we change to /Test
+                                                          // /test
+    
 });
 server.listen(3000);
