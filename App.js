b337a7dd1d04f0893338f4ae900951d626686e6a
ishitarastogi <ishita3099@gmail.com>
2020-10-14 23:32:57 +0530

    parsing bodies

--- App.js
+++ App.js
@@ -1,35 +1,37 @@
-const http= require('http');
-  //http module is a core module of Node.js
-//http module can be used globally...if absolut or relative path
-const fs=require('fs');                  //It is useful to perform all file I/O operations synchronously and asynchronously.           
-const server=http.createServer((req,res) => { // The request object can be used to get information 
-                                              //about the current HTTP request e.g., url, request header, and data. 
-                                             // The response object can be used to send a response for a current HTTP request. 
-    const url=req.url;
-    const method =req.method;
-     if(url==='/'){
-        res.setHeader('content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My first Node.js server</title></head>');
-        res.write('<body><form action="/message" method=POST> <input type="text"><button type="submit">SEND</button></input></form></body>')
-        res.write('</html>');   
-        return res.end();
-     }                                              //2. method:GET url:/ but if we change to /Test
-   
-     if(url==='/message' && method==='POST'){   
-        fs.writeFileSync('message.txt', 'Dummy');//create a new file 
-        res.writeHead(302, {'Location' : '/'});  //302: status code for redirection  2)set header method
-       return res.end();                         //writeHead() method. This method sends an 
-                                                //HTTP status code and a collection of response headers back to the client. 
-     }
-    // /test
-    res.setHeader('content-Type', 'text/html');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>My first Node.js server</title></head>');
-    res.write('<body>Hey!!This is Ishita. I created my first node.js server.</body>')
-    res.write('</html>');   
-    res.end();//can't write anything after this
-              //open developer tools -> network -> responses->headers
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# nodejs-proj
+---
+Parsing request bodies
+Streams and Buffers
+Incoming data is send as stream of data
+<img src ='streamsANDbuffers.png'/>
+To handle and manipulate streaming data like a video, a large file, etc., we need streams in Node. 
+streams work on a concept called buffer.
+
+A buffer is a temporary memory that a stream takes to hold some data until it is consumed.
+
+
+||||||||||||||||||||||||||||||||||||||||||||||||||||||
+The on method binds an event to a object.
+
+It is a way to express your intent if there is something happening, then execute the function added as a parameter. This style of programming is called Event-driven programming.
+---
